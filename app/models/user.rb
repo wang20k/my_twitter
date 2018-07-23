7a24e94965a1e66f70363152b8e9b6b84add3852
Kaylan <wang20k@ncssm.edu>
2018-07-23 10:02:13 -0400

    Day 2 commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  serialize :following, Array
+  has_many :tweets
+  mount_uploader :avatar, AvatarUploader
+
+  validates :username, presence: true, uniqueness: true
+end
